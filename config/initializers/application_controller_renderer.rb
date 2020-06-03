7bae41e9681f057fc59d3eeb7961d1bd252c6f82
ogi4997 <a14b4c4k17@gmail.com>
2020-06-04 01:49:56 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '483a0eaa205f6d18ce6a489287480b4ded7b3215c42f1db8e5e5d595da855467d12f9ee35dc6a3829dd54d9d4a3cdb412b04fa3408facdc2531ae35fd89d124e'
